76963a2864e3a3a3365ebf32b953d5d7d56e948d
Gabriela Lichy <105449193+gabrielalichy@users.noreply.github.com>
2022-05-16 18:19:07 -0300

    Update PrintFloatVariable.java

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable {
     public static void main(String[] args) {
         float PI = 3.14159f;
-        System.out.printf(PI, "Valor de pi: .%3f%n); 
+        System.out.printf(PI, "Valor de pi: .%3f%n"); 
         }  
 }
