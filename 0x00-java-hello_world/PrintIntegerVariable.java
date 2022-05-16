35b25e2b6554c141c60c4ab9d0462a5819e544b2
gabrielalichy <gabrielalichy@ciandt.com>
2022-05-16 14:48:00 -0300

    Print Integer Variable

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,9 @@
+public class PrintIntegerVariable {
+    public static void main(String[] args) {
+        int numero = 311;
+        String nome = "Avenida Andaló";
+
+        System.out.println(nome); 
+        System.out.println(numero);          
+    }
+}
