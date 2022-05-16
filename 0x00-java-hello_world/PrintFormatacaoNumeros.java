ce1da7a00cc98a83ef784bdb37194d1317e7c9d2
gabrielalichy <gabrielalichy@ciandt.com>
2022-05-16 19:16:22 -0300

    numeros

---
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -0,0 +1,8 @@
+public class PrintFormatacaoNumeros {
+    public static void main(String[] args) {
+        float taxa = 0.2456f;
+        float valor = 7654.321f;
+        System.out.printf("Valor: R$ %.2f", valor);
+        System.out.printf("\nTaxa: %.2f", taxa, "%" );
+    }
+}
