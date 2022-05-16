5115632e7d6885fda7149e64d71ff11ecbf34aff
gabrielalichy <gabrielalichy@ciandt.com>
2022-05-16 16:44:23 -0300

    correção linha

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,8 +1,6 @@
 public class PrintFloatVariable {
     public static void main(String[] args) {
         float PI = 3.14159f;
-    
-        System.out.printf("%.3f", PI);
-            
-    }  
+        System.out.printf("Valor de pi: %.3f", PI);
+      }  
 }
