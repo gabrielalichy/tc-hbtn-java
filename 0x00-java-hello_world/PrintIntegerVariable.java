8b6a11d7044aca436053e9840807ac977cf9a16c
gabrielalichy <gabrielalichy@ciandt.com>
2022-05-16 15:02:46 -0300

    correção

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -1,8 +1,8 @@
 public class PrintIntegerVariable {
     public static void main(String[] args) {
         int numero = 311;
-        String nome = "Avenida Andaló,";
+        String nome = "Avenida Andaló, ";
 
-        System.out.println(nome +  numero);        
+        System.out.println(nome + numero);        
     }
 }
