65c6d907d55506d6260aa22bc443bd779cb74f92
gabrielalichy <gabrielalichy@ciandt.com>
2022-05-17 18:10:21 -0300

    int

--- TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -1,13 +1,14 @@
 public class InteiroPositivo {
-    static boolean ehPrimo(int n)
+
+    static boolean ehPrimo(int valor)
     {
         // Corner case
-        if (n <= 1)
+        if (valor <= 1)
             return false;
 
         // Check from 2 to n-1
-        for (int i = 2; i < n; i++)
-            if (n % i == 0)
+        for (int i = 2; i < valor; i++)
+            if (valor % i == 0)
                 return false;
 
         return true;
