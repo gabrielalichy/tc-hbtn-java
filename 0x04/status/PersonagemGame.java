58e4a3f0e6f06ff50f94df518b3ab93125302632
gabrielalichy <gabrielalichy@ciandt.com>
2022-05-19 14:14:18 -0300

    status

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,45 @@
+public class PersonagemGame {
+	
+	public int saudeAtual;
+	public String nome;
+	private String status;
+		
+	public int getSaudeAtual() {
+		return saudeAtual;
+	}
+	public void setSaudeAtual(int saudeAtual) {
+		this.saudeAtual = saudeAtual;
+		if (saudeAtual > 0) {
+			status = "vivo";
+		} else {
+			status = "morto";
+		}
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void tomarDano(int quantidadeDeDano) {
+	saudeAtual = saudeAtual - quantidadeDeDano;
+	if(saudeAtual <= 0) {
+		saudeAtual = 0;
+	}
+	}
+	
+			
+	public void receberCura(int quantidadeDeCura){
+		saudeAtual = saudeAtual + quantidadeDeCura; 
+		if(saudeAtual >100) {
+			saudeAtual = 100;
+		}
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
