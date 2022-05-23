6864f2b231e47c3b8dbd8b062eba101c53658508
gabrielalichy <gabrielalichy@ciandt.com>
2022-05-23 13:31:30 -0300

    correcao1

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -8,9 +8,11 @@ public class Numeros0a99 {
 	}
 	public static void pilha() {
 		for (int i = 0; i <= 99; i++) {
+			if (i != 99) {
 			System.out.print(i + ", ");
-			if (i == 99) {
-				System.out.println();
+			} else {
+				
+				System.out.print(i + "\n");
 			}
 		}
 		
