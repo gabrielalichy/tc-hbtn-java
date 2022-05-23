01b1bd13a2392be872affe3bcdf56d5e3ebf43bf
gabrielalichy <gabrielalichy@ciandt.com>
2022-05-23 13:26:13 -0300

    correcao

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -7,11 +7,12 @@ public class Numeros0a99 {
 		
 	}
 	public static void pilha() {
-		List <Integer> listaInteiro = new ArrayList<>();
 		for (int i = 0; i <= 99; i++) {
-			listaInteiro.add(i);
+			System.out.print(i + ", ");
+			if (i == 99) {
+				System.out.println();
+			}
 		}
-		System.out.println(listaInteiro + "\n");
-				
+		
 	}
 }
