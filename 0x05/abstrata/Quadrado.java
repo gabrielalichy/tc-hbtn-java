bb45a4af77e099c2f6bdff9dd36ccf0b1bfa1ff2
gabrielalichy <gabrielalichy@ciandt.com>
2022-05-24 11:27:17 -0300

    abstrata

---
+++ 0x05/abstrata/Quadrado.java
@@ -0,0 +1,27 @@
+
+public class Quadrado extends Retangulo{
+	
+	private double lado;
+
+	public double getLado() {
+		return lado;
+	}
+
+	public void setLado(double lado) {
+		this.lado = lado;
+		if(lado <0) {
+			throw new java.lang.IllegalArgumentException("Lado deve ser maior ou igual a 0");
+		}else {
+			this.lado = lado;
+			this.largura = lado;
+			this.altura = lado;
+		}
+	}
+
+
+	@Override
+	public String toString() {
+		return "[Quadrado] " + String.format("%.2f" , lado);
+	}
+	
+}
