48c260f13e7d6c1cf1c6e878a8bf742a33a2a258
gabrielalichy <gabrielalichy@ciandt.com>
2022-05-26 13:44:45 -0300

    produtos

---
+++ 0x06/pedido_livraria/ItemPedido.java
@@ -0,0 +1,42 @@
+
+
+public class ItemPedido {
+
+	public int quantidade;
+	public Produto produtos;
+	public double precoBruto;
+
+	public double getPrecoBruto() {
+		precoBruto = produtos.getPrecoBruto();
+		return precoBruto;
+	}
+
+	public void setPrecoBruto(double precoBruto) {
+		this.precoBruto = precoBruto;
+	}
+
+	public ItemPedido(Produto produtos, int quantidade) {
+		this.quantidade = quantidade;
+		this.produtos = produtos;
+		this.precoBruto = getPrecoBruto();
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Produto getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(Produto produtos) {
+		this.produtos = produtos;
+	}
+
+
+
+}
