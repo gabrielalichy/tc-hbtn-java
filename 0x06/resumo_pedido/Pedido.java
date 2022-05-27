78fce9364f24ef1b81f33b8a0cab35ad9f5d1f68
gabrielalichy <gabrielalichy@ciandt.com>
2022-05-27 13:45:32 -0300

    resumo

--- 0x06/resumo_pedido/Pedido.java
+++ 0x06/resumo_pedido/Pedido.java
@@ -1,5 +1,3 @@
-
-
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
@@ -14,30 +12,27 @@ public class Pedido {
 	}
 	
 	public void apresentarResumoPedido() {
-			
 		double total = 0;
-//		 DecimalFormat decimalFormat = new DecimalFormat("###.##");
-//		 decimalFormat.setRoundingMode(RoundingMode.DOWN);
-		for (int i = 0; i < this.itens.length; i++) {
-			
-
+		double totalDesconto = 0;
+		DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");
+        decimalFormat.setRoundingMode(RoundingMode.HALF_UP);
 		System.out.println("------- RESUMO PEDIDO -------");
+		for (int i = 0; i < this.itens.length; i++) {
+
 		double valor = itens[i].getProdutos().obterPrecoLiquido();
 		double percentual = (itens[i].getProdutos().obterPrecoLiquido()*this.percentualDesconto)/100;
 		total = total +  ( valor * itens[i].getQuantidade()) - ( percentual * itens[i].getQuantidade());
-		System.out.println("Tipo: "+ itens[i].getProdutos().getClass().getSimpleName()  +  " " + "Titulo: "  + (itens[i].getProdutos().getTitulo()) + " Preco:  " +  manterCasas( total,2) + " " +  "Quant: " + (itens[i].getQuantidade() + " Total: " +  manterCasas( total,2)));
-		System.out.println("----------------------------");
-		System.out.println("DESCONTO: " +  manterCasas( ((itens[i].getProdutos().obterPrecoLiquido()*this.percentualDesconto)/100),2));
-		}	
+		totalDesconto =totalDesconto + (percentual * itens[i].getQuantidade());
+		System.out.println("Tipo: "+ itens[i].getProdutos().getClass().getSimpleName()  +  " " + "Titulo: "  + (itens[i].getProdutos().getTitulo()) + " Preco:  " +  manterCasas( itens[i].getProdutos().obterPrecoLiquido(),2) + " " +  "Quant: " + (itens[i].getQuantidade() + " Total: " +  manterCasas( itens[i].getProdutos().obterPrecoLiquido(),2)));
 		
-			System.out.println("Total Produto: "+manterCasas(total,2));
+		}	
+		System.out.println("----------------------------");
+		double valorTotal =  total +  totalDesconto;
+		    System.out.println("DESCONTO: " + decimalFormat.format(totalDesconto));
+			System.out.println("TOTAL PRODUTO: "+  decimalFormat.format(valorTotal));
 			System.out.println("----------------------------");
 			System.out.println("TOTAL PEDIDO:: " +manterCasas( total,2));
 			System.out.println("----------------------------");
-		
-		
-
-
 	}
 	static double manterCasas(double valor, int qtdCasas) {
 		double fator = (double) Math.pow(10, qtdCasas);
@@ -60,3 +55,4 @@ public class Pedido {
 	}
 
 }
+
