e2079fe4580212f88cfe22b59323e2326741339b
gabrielalichy <gabrielalichy@ciandt.com>
2022-06-02 08:29:37 -0300

    att

--- Enum/celular/Celular.java
+++ Enum/celular/Celular.java
@@ -2,13 +2,12 @@ import java.util.ArrayList;
 
 public class Celular {
 
-	private ArrayList<Contato> contatos = new ArrayList<Contato>();
-	
+	private ArrayList<Contato> contato = new ArrayList();
 	private boolean jaexiste = false;
 
 	private Integer obterPosicaoContato(String nomeContato) {
-		for (int i = 0; i < contatos.size(); i++) {
-			if (contatos.get(i).getNome().equals(nomeContato)) {
+		for (int i = 0; i < contato.size(); i++) {
+			if (contato.get(i).getNome().equals(nomeContato)) {
 				return i;
 			}
 		}
@@ -21,9 +20,9 @@ public class Celular {
 
 		if (retorno > -1) {
 			throw new IllegalArgumentException(
-					"Nao foi possivel adicionar contato. Contato jah existente com esse nome");
+					"Nao foi possivel adicionar contato. Contato jah existente com esse nome.");
 		} else {
-			this.contatos.add(addContato);
+			this.contato.add(addContato);
 		}
 	}
 
@@ -33,18 +32,18 @@ public class Celular {
 		Integer retornoNovo = obterPosicaoContato(novoContato.getNome());
 		if (retorno <= -1) {
 			jaexiste = true;
-			throw new IllegalArgumentException("Nao foi possivel modificar contato. Contato nao existe");
+			throw new IllegalArgumentException("Nao foi possivel modificar contato. Contato nao existe.");
 			
 		}
 		if (retornoNovo > -1 && jaexiste == true) {
 			throw new IllegalArgumentException(
-					"Nao foi possivel adicionar contato. Contato jah existente com esse nome");
+					"Nao foi possivel modificar contato. Contato jah existente com esse nome.");
 		}
 
 		if (retorno > -1) {
-			for (int i = 0; i < this.contatos.size(); i++) {
-				if (contatos.get(i).nome.equals(novoContato.getNome())) {
-					contatos.set(i, novoContato);
+			for (int i = 0; i < this.contato.size(); i++) {
+				if (contato.get(i).nome.equals(novoContato.getNome())) {
+					contato.set(i, novoContato);
 
 				}
 			}
@@ -57,18 +56,18 @@ public class Celular {
 		Integer retorno = obterPosicaoContato(contato.getNome());
 
 		if (retorno <= -1) {
-			throw new IllegalArgumentException("Nao foi possivel remover contato. Contato nao existe");
+			throw new IllegalArgumentException("Nao foi possivel remover contato. Contato nao existe.");
 		} else {
-			for (int i = 0; i < this.contatos.size(); i++) {
-				this.contatos.remove(i);
+			for (int i = 0; i < this.contato.size(); i++) {
+				this.contato.remove(i);
 			}
 		}
 
 	}
 
 	public void listarContatos() {
-		for (Contato contato2 : this.contatos) {
-			System.out.println(contato2.getNome() + " -> " + contato2.getNumero() + " (" + contato2.getTipo() + ")");
+		for (Contato contato2 : this.contato) {
+			System.out.println(contato2.getNome() + " -> " + contato2.getNumero() + "(" + contato2.getTipo() + ")");
 		}
 
 	}
