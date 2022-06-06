08e8b9dc8cf844dbe8a5bc8ac4cd249d71491496
gabrielalichy <gabrielalichy@ciandt.com>
2022-06-06 09:44:44 -0300

    hashmap

--- Set_Map/telefones/ListaTelefonica.java
+++ Set_Map/telefones/ListaTelefonica.java
@@ -3,23 +3,52 @@ import java.util.HashMap;
 
 public class ListaTelefonica {
 
-	HashMap<String, Telefone> pessoas = new HashMap<String, Telefone>();
+	HashMap<String, Telefone> pessoas;
 	ArrayList<Telefone> telefone;
 	private HashMap nome;
 
-	public ListaTelefonica(HashMap nome) {
+	public ListaTelefonica() {
 		this.nome = nome;
+		pessoas = new HashMap<String, Telefone>();
+
 	}
-	
-	
-	public void adicionarTelefone() {
+
+	public void adicionarTelefone(String nome, Telefone telefone1) {
+		pessoas.put(nome, telefone1);
+
+	}
+
+	public ArrayList<Telefone> buscar(String nome) {
+		ArrayList<Telefone> buscaTelefone = new ArrayList<Telefone>();
+		for (String telefone : pessoas.keySet()) {
+			System.out.println(telefone);
+		}
+
+		return telefone;
+	}
+
+	public HashMap<String, Telefone> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(HashMap<String, Telefone> pessoas) {
 		this.pessoas = pessoas;
+	}
+
+	public ArrayList<Telefone> getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(ArrayList<Telefone> telefone) {
 		this.telefone = telefone;
 	}
-	
-	public void buscar() {
-		
+
+	public HashMap getNome() {
+		return nome;
 	}
-	
-	
+
+	public void setNome(HashMap nome) {
+		this.nome = nome;
+	}
+
 }
--- Set_Map/telefones/Telefone.java
+++ Set_Map/telefones/Telefone.java
@@ -2,6 +2,35 @@
 public class Telefone {
 
 	public String codigoArea;
-	public int numero;
+	public String numero;
+
 	
+	public Telefone(String codigoArea, String numero) {
+		this.codigoArea = codigoArea;
+		this.numero = numero;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "[(" + getCodigoArea() + ") " + getNumero() + ", ";
+	}
+
+
+	public String getCodigoArea() {
+		return codigoArea;
+	}
+
+	public void setCodigoArea(String codigoArea) {
+		this.codigoArea = codigoArea;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
 }
