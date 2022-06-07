5aec616e8726c44dde1b21376bb7c6a7e1f216ac
gabrielalichy <gabrielalichy@ciandt.com>
2022-06-07 11:31:49 -0300

    collecions

--- Set_Map/telefones_duplicados/ListaTelefonica.java
+++ Set_Map/telefones_duplicados/ListaTelefonica.java
@@ -1,58 +1,68 @@
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map.Entry;
-import java.util.Set;
 
 public class ListaTelefonica {
 
-	HashMap<String, ArrayList<Telefone>> pessoas;
-	HashSet<Telefone> telefone = new HashSet<>();;
+	HashMap<String, HashSet<Telefone>> pessoas;
+	HashSet<Telefone> telefone = new HashSet<>();
 
 	public ListaTelefonica() {
-		pessoas = new HashMap<String, ArrayList<Telefone>>();
+		pessoas = new HashMap<String, HashSet<Telefone>>();
 
 	}
 
-	public HashSet<Telefone> adicionarTelefone(String nome, Telefone telefone1) {
-		return telefone;
+	public HashSet<Telefone>  adicionarTelefone(String nome, Telefone telefone1) {
+
 		boolean isKeyPresent = this.pessoas.containsKey(nome);
+		HashSet<Telefone> teste =new HashSet<>();
+		teste.add(telefone1);
 		if (isKeyPresent) {
-			telefone.add(telefone1);
-			this.pessoas.keySet();
-		}else {
-			telefone = new HashSet<>();;
-			telefone.add(telefone1);
-			this.pessoas.equals(getTelefone());
+			for (Telefone t : telefone) {
+			if (t.equals(telefone1)) {
+				try {
+					throw new Exception();
+				} catch (Exception e) {
+					System.out.println("Telefone jah existente para essa pessoa.");
+					System.out.println("Telefone jah pertence a outra pessoa.");
+					return telefone;
+				}
+			}
+			Collection<HashSet<Telefone>> teste2 = this.pessoas.values(); 
+			if (teste2.contains(telefone1)) {
+				return telefone;
+			}	
 		}
+				
+			telefone.add(telefone1);
+			this.pessoas.put(nome,  telefone);
+	
+	}
+		else {
+			telefone = new HashSet<>();
+			telefone.add(telefone1);
+			this.pessoas.put(nome,  telefone);
+		}
+		return telefone;
 		
 	}
-
-	public Set<Telefone> buscar(String nome) {
-		ArrayList<Telefone> buscaTelefone = new ArrayList<Telefone>();
-		for (Entry<String, ArrayList<Telefone>> entrada : pessoas.entrySet()) {
+	public HashSet<Telefone> buscar(String nome) {
+		HashSet<Telefone> buscaTelefone = new HashSet<Telefone>();
+		for (Entry<String,HashSet<Telefone> > entrada : pessoas.entrySet()) {
 			
 		    if (entrada.getKey() == nome) {
 		    		buscaTelefone.addAll(entrada.getValue());
+		    		return buscaTelefone;
 			} 
 		}if (buscaTelefone.size() > 0 ) {
-			return (Set<Telefone>) buscaTelefone;
+			return buscaTelefone;
 			}
 		else {
 			return null;
 		}
 	}
 
-
-
-	public HashSet<Telefone> getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(HashSet<Telefone> telefone) {
-		this.telefone = telefone;
-	}
-
-	
 
 }
