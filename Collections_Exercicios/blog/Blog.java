1f916f3664a8d6204f59fa4a02b81345720a9a72
gabrielalichy <gabrielalichy@ciandt.com>
2022-06-09 08:58:54 -0300

    corpo blog

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Blog {
+
+	ArrayList<Post> post = new ArrayList<Post>();
+
+	public void adicionarPostagem() {
+		
+	}
+	
+	public void obterTodosAutores() {
+		
+	}
+	
+	public void obterContagemPorCategoria() {
+		
+	}
+	
+}
