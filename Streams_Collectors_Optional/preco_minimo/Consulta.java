8852c557f5c5620f4c7e11d3ff7f8a6e1622c948
gabrielalichy <gabrielalichy@ciandt.com>
2022-06-13 15:14:13 -0300

    Relatório Produtos por preco minimo

---
+++ Streams_Collectors_Optional/preco_minimo/Consulta.java
@@ -0,0 +1,28 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+	public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+
+		List<Produto> produtoPedido = pedido.getProdutos();
+
+		return produtoPedido.stream().filter(p -> p.getCategoria().equals(CategoriaProduto.LIVRO))
+				.collect(Collectors.toList());
+	}
+
+	public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+
+		Optional<Produto> maiorPreco = produtos.stream().max(Comparator.comparingDouble(Produto::getPreco));
+
+		return maiorPreco.orElse(null);
+	}
+
+	public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+
+		return produtos = produtos.stream().filter(p -> p.getPreco() >= precoMinimo).collect(Collectors.toList());
+
+	}
+}
